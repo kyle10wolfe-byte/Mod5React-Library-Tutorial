12e221f70425cff063503bb8e0933a7d0d143591
kyle10wolfe-byte <kyle10wolfe@gmail.com>
2026-02-09 17:15:08 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Highlights from './components/Highlights';
+import Landing from './components/Landing';
+import Nav from './components/Nav';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Landing />
+      <Highlights />
+    </div>
+  );
+}
+
+export default App;
+
+
+
