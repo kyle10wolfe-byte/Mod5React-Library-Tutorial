11d445691e44db6069b76dd35c4ebd8dbdec95a9
kyle10wolfe-byte <kyle10wolfe@gmail.com>
2026-02-10 15:32:45 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import Nav from '.src/components/nav.jsx';
+import Highlights from './components/Highlights';
+import Landing from './components/Landing';
+import Nav from './components/Nav';
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
     </div>
   );
 }
 
 export default App;
+
+
+
